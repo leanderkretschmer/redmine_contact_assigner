7ca0d2ad3115fc499e5b68bf6a98a13057f7b748
leanderkretschmer <106610902+leanderkretschmer@users.noreply.github.com>
2025-11-25 14:37:31 +0100

    fixed view error

--- lib/redmine_contact_assigner/hooks.rb
+++ lib/redmine_contact_assigner/hooks.rb
@@ -1,7 +1,13 @@
 module RedmineContactAssigner
   class Hooks < Redmine::Hook::ViewListener
-    render_on :view_issues_form_details_bottom,
-              partial: 'hooks/redmine_contact_assigner/view_issues_form_details_bottom'
+    def view_issues_form_details_bottom(context = {})
+      return '' unless context[:f]
+      context[:controller].send(:render_to_string, partial: 'hooks/redmine_contact_assigner/view_issues_form_details_bottom', locals: context)
+    end
+
+    def view_issues_show_details_bottom(context = {})
+      context[:controller].send(:render_to_string, partial: 'hooks/redmine_contact_assigner/view_issues_show_details_bottom', locals: context)
+    end
 
     def controller_issues_new_before_save(context = {})
       upsert_assigned_contact(context)
